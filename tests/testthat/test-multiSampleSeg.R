a0b9884db22d33fab2136a4dcd25fdbe74553b44
Toby Dylan Hocking <tdhock5@gmail.com>
2015-03-11 16:45:52 -0400

    multiSampleSeg test fails

---
+++ tests/testthat/test-multiSampleSeg.R
@@ -0,0 +1,18 @@
+context("multiSampleSeg")
+
+count <-
+  c(rep(c(0, 1), each=5),
+    rep(c(10, 11), each=10),
+    rep(c(0, 1), each=5))
+
+test_that("with 1 sample we refine peaks" , {
+  chromEnd <- seq_along(count)
+  profiles <-
+    data.frame(chromStart=chromEnd-1L,
+               chromEnd,
+               sample.id="sample1",
+               count)
+  peak <- multiSampleSeg(profiles)
+  expect_equal(peak$chromStart, 10)
+  expect_equal(peak$chromEnd, 30)
+})
