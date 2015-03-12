6ecc8097d035af6765f4cbc3235cf59e5f212001
Toby Dylan Hocking <tdhock5@gmail.com>
2015-03-12 13:51:25 -0400

    compute bin mat

--- src/multiSampleSeg.c
+++ src/multiSampleSeg.c
@@ -36,7 +36,8 @@ multiSampleSeg(
   if(bases <= n_bins){
     return ERROR_FEWER_BASES_THAN_BINS;
   }
-  int bin_i, bin_chromStart, bin_chromEnd;
+  int bin_i, bin_chromStart, bin_chromEnd, bin_done, bin_count,
+    add_bases, add_chromStart, add_chromEnd, add_coverage;
   int *bin_mat = (int*) malloc(n_bins * n_samples * sizeof(int));
   for(sample_i=0; sample_i < n_samples; sample_i++){
     profile = samples[sample_i];
@@ -44,7 +45,56 @@ multiSampleSeg(
     for(bin_i=0; bin_i < n_bins; bin_i++){
       bin_chromStart = bin_i * bases/n_bins + max_chromStart;
       bin_chromEnd = (bin_i+1)*bases/n_bins + max_chromStart;
-      printf("[%3d] (%10d, %10d]\n", bin_i, bin_chromStart, bin_chromEnd);
+      bin_count = 0;
+      bin_done = 0;
+      while(!bin_done){
+	//      (  bin  ]
+	//             ( profile ]
+	//      (          bin             ]
+	// There is at least 1 base to add to this bin.
+	if(profile->chromStart[coverage_i] < bin_chromStart){
+	  //    (  bin  ]
+	  // ( profile ]
+	  //   ( profile ]
+	  add_chromStart = bin_chromStart;
+	}else{
+	  //    (  bin  ]
+	  //    ( profile ]
+	  //     ( profile ]
+	  add_chromStart = profile->chromStart[coverage_i];
+	}
+	if(profile->chromEnd[coverage_i] <= bin_chromEnd){
+	  //  ( profile ]
+	  //      ( bin ]
+	  //       ( bin ]
+	  //           ( bin ]
+	  add_chromEnd = profile->chromEnd[coverage_i];
+	  // add until the end of this profile segment, 
+	  // so we are done with it.
+	  add_coverage = 1;
+	}else{
+	  //  ( profile ]
+	  //     ( bin ]
+	  //    ( bin ]
+	  add_chromEnd = bin_chromEnd;
+	  add_coverage = 0;
+	}
+	add_bases = add_chromEnd-add_chromStart;
+	bin_count += profile->coverage[coverage_i] * add_bases;
+	coverage_i += add_coverage;
+	if(bin_chromEnd <= profile->chromEnd[coverage_i]){
+	  //  ( profile ]
+	  //      ( bin ]
+	  //     ( bin ]
+	  //    ( bin ]
+	  bin_done = 1;
+	}
+      }//while
+      /*
+      printf("sample_i=%d bin_i=%3d (%10d, %10d] %d\n", 
+	     sample_i, bin_i, bin_chromStart, bin_chromEnd, bin_count);
+      */
+      bin_mat[bin_i + sample_i*n_bins] = bin_count;
     }
   }
   free(bin_mat);
