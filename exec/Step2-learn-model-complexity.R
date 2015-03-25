8035ee4807cc143759e24c1b8faaa996cec5be9e
Toby Dylan Hocking <tdhock5@gmail.com>
2015-03-25 18:47:16 -0400

    write html report

--- exec/Step2-learn-model-complexity.R
+++ exec/Step2-learn-model-complexity.R
@@ -1,4 +1,5 @@
 library(data.table)
+library(xtable)
 library(ggplot2)
 library(PeakSegDP)
 
@@ -12,7 +13,7 @@ RData.files <- Sys.glob(file.path(no.trailing, "*", "*.RData"))
 cat("Input files:\n")
 print(RData.files)
 stopifnot(length(RData.files) > 0)
-names(RData.files) <- sub("[.]RData$", "", basename(RData.files))
+names(RData.files) <- sub("_residuals[.]RData$", "", basename(RData.files))
 out.RData <- file.path(no.trailing, "learned.model.RData")
 cat("Output file:\n", out.RData, "\n", sep="")
 
@@ -20,7 +21,7 @@ error.list <- list()
 features.limits.list <- list()
 for(sample.id in names(RData.files)){
   RData.file <- RData.files[[sample.id]]
-  load(RData.file)
+  objs <- load(RData.file)
   error.list[[sample.id]] <- data.table(sample.id, errors)
   features.limits.list[[sample.id]] <- features.limits
 }
@@ -32,17 +33,6 @@ res.errors <-
            total.weight=sum(total.weight)),
          by=bases.per.bin]
 
-errPlot <- 
-ggplot(res.errors, aes(bases.per.bin, weighted.error))+
-  geom_line()+
-  scale_x_log10()+
-  geom_point()
-
-error.pdf <- file.path(no.trailing, "figure-weightedError.pdf")
-pdf(error.pdf)
-print(errPlot)
-dev.off()
-
 ## consider only resolutions with max weights.
 max.weight.value <- max(res.errors$total.weight)
 max.weight <- res.errors[total.weight == max.weight.value, ]
@@ -52,9 +42,111 @@ min.err.value <- min(max.weight$weighted.error)
 min.err <- max.weight[weighted.error == min.err.value, ]
 
 ## The largest resolution will take the least amount of CPU time.
-bases.per.bin <- max(min.err$bases.per.bin)
+bases.per.bin.dt <- min.err[which.max(bases.per.bin), ]
+bases.per.bin <- bases.per.bin.dt$bases.per.bin
 bases.per.bin.str <- paste(bases.per.bin)
 
+y.range <- max(max.weight$weighted.error)-min.err.value
+
+errPlot <- 
+ggplot()+
+  ggtitle("selecting the best resolution")+
+  geom_line(aes(bases.per.bin, weighted.error), data=max.weight)+
+  geom_point(aes(bases.per.bin, weighted.error), data=bases.per.bin.dt)+
+  geom_text(aes(bases.per.bin, weighted.error-y.range/20,
+                label=paste(bases.per.bin, "bases/bin")),
+            hjust=1,
+            data=bases.per.bin.dt)+
+  scale_x_log10()+
+  theme_grey()
+
+error.base <- "figure-weightedError.png"
+error.png <- file.path(no.trailing, error.base)
+png(error.png, units="in", res=100, width=5, height=3)
+print(errPlot)
+dev.off()
+
+errors.by.res <- split(errors, errors$bases.per.bin)
+errors.best.res <- errors.by.res[[bases.per.bin.str]]
+errors.ordered <- errors.best.res[order(weighted.error, decreasing = TRUE), ]
+errors.ordered[, sample.path := sub("_residuals[.]RData$", "",
+                               RData.files[sample.id])]
+errors.ordered[, relative.path := sub(paste0(no.trailing, "/"),
+                                 "", sample.path)]
+errors.ordered[, problem.base := file.path(relative.path,
+                                bases.per.bin, chrom, problem.name)]
+errors.ordered[, thumb.png := paste0(problem.base, "-thumb.png")]
+errors.ordered[, index := file.path(problem.base, "index.html")]
+errors.ordered[, thumb.href := sprintf('<a href="%s"><img src="%s" /></a>',
+                              index, thumb.png)]
+errors.ordered[, chrom.fac := factor(chrom, paste0("chr", c(1:22, "X", "Y")))]
+errors.ordered[, chromRange := sub(".*:", "", problem.name)]
+errors.ordered[, chromEnd := as.integer(sub(".*-", "", chromRange))]
+errors.ordered[, chromStart := as.integer(sub("-.*", "", chromRange))]
+errors.ordered[, errors := ifelse(weighted.error == 0, "none", "some")]
+
+## TODO: add chrom diagram viz with 
+## ggplot()+
+##   geom_point(aes(chromEnd, chrom.fac,
+##   href=index, color=errors), data=errors.ordered)+
+##   scale_color_manual(values=c(none="grey", some="black"))+
+##   facet_grid(sample.id ~ .)
+ann.colors <-
+  c(noPeaks="#f6f4bf",
+    peakStart="#ffafaf",
+    peakEnd="#ff4c4c",
+    peaks="#a445ee")
+rcopy <- data.table(regions)
+rcopy[, chrom.fac := factor(paste(chrom), paste0("chr", c(1:22, "X", "Y")))]
+tit <-
+  paste0("training data:\n",
+         nrow(regions), " labels per sample (color),\n",
+         length(RData.files), " samples (", sample.id, " shown),\n",
+         length(unique(regions$chunk.id)), " chunks,\n",
+         length(unique(errors.ordered$problem.name)),
+         " segmentation problems (black)")
+chromPlot <- 
+ggplot()+
+  ggtitle(tit)+
+  theme_grey()+
+  ylab("chromosome")+
+  xlab("position on chromosome (kilo bases = kb)")+
+  geom_segment(aes(chromStart/1e3, chrom.fac,
+                   xend=chromEnd/1e3, yend=chrom.fac),
+               size=2,
+               data=errors.ordered)+
+  geom_point(aes(chromEnd/1e3, chrom.fac), data=errors.ordered,
+             size=3)+
+  scale_color_manual(values=ann.colors)+
+  geom_segment(aes(chromStart/1e3, chrom.fac,
+                   color=annotation,
+                   xend=chromEnd/1e3, yend=chrom.fac),
+               size=1,
+               data=rcopy)+
+  geom_point(aes(chromEnd/1e3, chrom.fac, color=annotation),
+             size=2,
+             data=rcopy)
+chrom.base <- "figure-training-data-chroms.png"
+chrom.path <- file.path(no.trailing, chrom.base)
+png(chrom.path, units="in", res=100, width=10, height=8)
+print(chromPlot)
+dev.off()
+
+
+xt.cols <-
+  c("thumb.href", "problem.name",
+    "weighted.error", "total.weight",
+    "min.peaks", "max.peaks")
+errors.df <- data.frame(errors.ordered)[, xt.cols]
+xt <- xtable(errors.df)
+
+report.file <- file.path(no.trailing, "learned.model.html")
+cat(sprintf('<img src="%s" /> <br />', chrom.base),
+    sprintf('<img src="%s" /> <br />', error.base),
+    file=report.file)
+print(xt, type="html", file=report.file, append=TRUE,
+      sanitize.text.function=identity)
+cat("Wrote result report to ", report.file, "\n", sep="")
 ## interesting cases for training:
 
 ## only 1 sample, but several chunks => train on a few chunks,
@@ -171,8 +263,9 @@ with.facets <-
   facet_grid(validation.fold ~ ., labeller=function(var, val){
     paste("fold", val)
   })
-modelSelection.pdf <- file.path(no.trailing, "figure-modelSelection.pdf")
-pdf(modelSelection.pdf)
+modelSelection.base <- "figure-modelSelection.png"
+modelSelection.png <- file.path(no.trailing, modelSelection.base)
+png(modelSelection.png, units="in", res=200, width=15, height=4)
 print(with.facets)
 dev.off()
 
