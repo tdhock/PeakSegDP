6b88b0bd0bab29d275f3e17d5977d62650a0d989
Toby Dylan Hocking <tdhock5@gmail.com>
2015-03-31 11:09:20 -0400

    chromProblems fun

---
+++ R/chromProblems.R
@@ -0,0 +1,46 @@
+chromProblems <- function
+### Divide a chromosome into a set of overlapping segmentation
+### problems.
+(first.chromStart,
+### Base before the first base on this chromosome.
+ last.chromEnd,
+### Last base on this chromosome.
+ bases.per.problem
+### How many bases in each segmentation problem?
+ ){
+  stopifnot(is.integer(first.chromStart))
+  stopifnot(is.integer(last.chromEnd))
+  stopifnot(is.integer(bases.per.problem))
+  stopifnot(length(first.chromStart)==1)
+  stopifnot(length(last.chromEnd)==1)
+  stopifnot(length(bases.per.problem)==1)
+  problemEnd <-
+    as.integer(seq(0, last.chromEnd, by=bases.per.problem/2)[-(1:3)])
+  if(length(problemEnd) == 0){
+    problemEnd <- peakEnd <- last.chromEnd
+    problemStart <- peakStart <- first.chromStart
+    problems <- 
+      data.table(chromStart=problemStart,
+                 peakStart,
+                 peakEnd,
+                 chromEnd=problemEnd)
+  }else{
+    problemStart <- as.integer(problemEnd-bases.per.problem)
+    peakStart <- as.integer(problemStart + bases.per.problem/4)
+    peakEnd <- as.integer(problemEnd - bases.per.problem/4)
+    problems <- 
+      data.table(chromStart=as.integer(c(0,
+                   problemStart, problemEnd[length(problemEnd)-1])),
+                 peakStart=as.integer(c(0,
+                   peakStart, peakEnd[length(peakEnd)])),
+                 peakEnd=as.integer(c(peakStart[1], peakEnd, last.chromEnd)),
+                 chromEnd=as.integer(c(bases.per.problem,
+                   problemEnd, last.chromEnd)))
+  }
+  problems[, problem.name :=
+             sprintf("%s:%09d-%09d", chrom, chromStart, chromEnd)]
+  problem.before <- problems$chromEnd < first.chromStart
+  problems[!problem.before, ]
+### data.table with columns chromStart, peakStart, peakEnd, chromEnd,
+### problem.name.
+}
