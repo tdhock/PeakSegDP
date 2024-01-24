618128bb8163323bfa68ad75e24f5ca790b61371
Toby Dylan Hocking <Toby.Hocking@nau.edu>
2024-01-24 11:48:29 -0700

    fix for CRAN

--- build.sh
+++ build.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
+set -o errexit
+PREGEX="^Package: "
+PKG=$(grep $PREGEX DESCRIPTION|sed "s/$PREGEX//")
+echo Package from DESCRIPTION: $PKG
 cd ..
-rm -rf PeakSegDP-release
-cp -r PeakSegDP PeakSegDP-release
-#grep -v Remotes PeakSegDP/DESCRIPTION > PeakSegDP-release/DESCRIPTION
-PKG_TGZ=$(R CMD build PeakSegDP-release|grep building|sed 's/.*‘//'|sed 's/’.*//')
-R CMD INSTALL $PKG_TGZ
-R CMD check --as-cran $PKG_TGZ
+
+RELEASE=$PKG-release
+echo Copying $PKG to $RELEASE
+rm -rf $RELEASE
+cp -r $PKG $RELEASE
+
+echo Editing $RELEASE for CRAN submission
+grep -v Remotes $PKG/DESCRIPTION > $RELEASE/DESCRIPTION
+
+echo Building $RELEASE
+RCMD="R --vanilla CMD"
+$RCMD build $RELEASE | tee build.out
+PKG_TGZ=$(grep building build.out|sed "s/.*\($PKG.*.tar.gz\).*/\1/")
+
+echo Installing $PKG_TGZ
+$RCMD INSTALL $PKG_TGZ
+
+echo Checking $PKG_TGZ
+$RCMD check --as-cran $PKG_TGZ
